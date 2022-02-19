44a8b47c245792f1762376b5cd9873bcb142a776
niiwade <niilantewade77@gmail.com>
2022-02-19 08:16:59 +0000

    started project

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: "jit",
+  purge: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['src/**/*.{html,css,js,jsx,ts,tsx,svelte}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
