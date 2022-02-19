18c51f71856ec16b6dd8c8ce61d8f4853d1d54a2
niiwade <niilantewade77@gmail.com>
2022-02-19 11:18:01 +0000

    writing style for the side nav bar and adede icons

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,20 @@
 <div class="wrap">
-	<div class="nav-side">
+	<div class="side-nav">
 		<div class="logo">
 			<img alt="main logo" src="src/assests/logo.png" />
 		</div>
+
+		<nav class="menu">
+			<ul>
+				<li><a alt="" href="#"><img src="src/assests/graph.svg" alt="" /></a></li>
+				<li><a href="#"><img src="src/assests/app.svg" alt="" /></a></li>
+				<li><a href="#"><img src="src/assests/calendar.svg" alt="" /></a></li>
+				<li><a href="#"><img src="src/assests/folder.svg" alt="" /></a></li>
+				<li><a href="#"><img src="src/assests/messages.svg" alt="" /></a></li>
+				<li><a href="#"><img src="src/assests/settings.svg" alt="" /></a></li>
+			</ul>
+		</nav>
 	</div>
+
+	<div class="content" />
 </div>
