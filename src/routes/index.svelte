4689c358a9c563f701fcd0c0460af942afd64c0b
niiwade <niilantewade77@gmail.com>
2022-02-22 19:51:24 +0000

    change

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,7 +6,14 @@
 
 		<nav class="menu">
 			<ul>
-				<li><a alt="" href="#"><img src="src/assests/graph.svg" alt="" /></a></li>
+				<li class="active">
+					<a alt="" href="#">
+						<img src="src/assests/graph.svg" alt="" />
+						<span class="stroke">
+							<span class="half" />
+						</span>
+					</a>
+				</li>
 				<li><a href="#"><img src="src/assests/app.svg" alt="" /></a></li>
 				<li><a href="#"><img src="src/assests/calendar.svg" alt="" /></a></li>
 				<li><a href="#"><img src="src/assests/folder.svg" alt="" /></a></li>
