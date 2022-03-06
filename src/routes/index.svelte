e35720c1a480ee3e08f80d0cd89c3847bc62038d
niiwade <niilantewade77@gmail.com>
2022-03-06 01:47:47 +0000

    fixes on stats

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -154,7 +154,7 @@
 								<div class="monthly-stats">
 									<h1 class="h-title">Stats on May</h1>
 									<div class="stats-chart">
-										<svg viewBox="0 0 36 36">
+										<svg viewBox="0 0 100 100">
 											<path
 												d="M18 2.0845 
 											a 15.9155 15.9155 0 0 1 0 31.831
